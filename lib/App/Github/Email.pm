eb36527afc1aa83df297dfd6853c63622b467e18
faraco <skelic3@gmail.com>
2017-01-16 16:01:01 +0800

    v0.0.1

---
+++ lib/App/Github/Email.pm
@@ -0,0 +1,20 @@
+use strict;
+use warnings;
+package App::Github::Email;
+#ABSTRACT: Search and print related Github user emails.
+# VERSION
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+	github-email [-n|-name|--name] <USERNAME> # => output related Github user emails (from event)
+
+=head1 DESCRIPTION
+
+Search and print Github related user emails from public event.
+
+=cut
+1;
